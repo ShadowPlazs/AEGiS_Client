092a25f4303424bb5329e8e33e3e48530228adf8
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2022-01-05 10:48:15 +0800

    Update RSoD.py

--- RSoD.py
+++ RSoD.py
@@ -1,1 +1,9 @@
+import rsod
 
+basic.showImageid("4329867")
+printScreen("Your PC had a fatal error and needs to restart.")
+printScreen("Code: ERROR_FATAL_DETECTED")
+del(10)
+printScreen("Restarting your PC...")
+basic.showAniImageid("38322121")
+restart("LocalDisk (C:/) / PC")
