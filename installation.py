e7d56f23793f12fe06d72244fd01821a4633f5bd
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2022-01-06 21:03:32 +0800

    Create installation.py

---
+++ installation.py
@@ -0,0 +1,11 @@
+import installation
+from installation import advanced
+from installation => advanced import UI
+
+# Starting Stage [Getting Things Ready]
+
+showGUI("Getting Things Ready",button1("Install",1947=https://aegis.com/software-download/aegis-client@64.format(Install)),button2("Cancel",316353=C:/Task/Cancel/AEGiS_Client_Installation (64-bit).format(Cancel))
+        
+# Installasion Stage [Installing AEGiS Client]
+        
+showGUI("Installing AEGiS Client",button1("Cancel"=C:/Task =/Cancel/AEGiS_Client_Installation (64-bit).format(Cancel))
