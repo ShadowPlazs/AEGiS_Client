e4f67ed007892788991d7ef0e08ac5224313d752
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2022-01-08 11:07:27 +0800

    Update SystemAutos.py

--- SystemAutos.py
+++ SystemAutos.py
@@ -5,17 +5,17 @@ import auto
 if task.killed("Isass"):
   print("You are about to be logged off.","Please save your work and log off immediadly. The system process has ran into an error and we have reported it to AEGiS Headquarters®. Your PC will shutdown in 1 minute.")
   nop(60)
-  taskKill("SYSTEM_PROCESS")
+  taskKill(/F,"SYSTEM_PROCESS")
   set("RSoD",true)
   
 if task.killed("AEG@Defender"):
   print("You are about to be logged off.","Please save your work and restart immediadly. The AEGiS Desktop Defender has suddenly crashed. Please restart your computer. Your PC will auto-restart 1 minute later.")
   nop(60)
-  taskKill("SYSTEM_PROCESS")
+  taskKill(/IM,"SYSTEM_PROCESS")
   set("RSoD",true)
   
 if task.killed("SYSTEM_PROCESS"):
   set("RSoD",true)
   
 if task.killed("AEGiS_Contacter"):
-  print("0.emote you don't have permission to crash this process.".format(EMOTE10))
+  print("0.emote You don't have permission to crash this process.".format(EMOTE10))
