4a3f5284a229166f4518afea84dafe01bc75239e
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2022-01-08 14:08:02 +0800

    Create UsrConfiguration.py

---
+++ User/UsrConfiguration.py
@@ -0,0 +1,14 @@
+import config
+from config import usr
+
+def on_error():
+  print("A configuration error has been detected and we will get right to it.")
+  printScreen("A configuration error has been detected and we will get right to it.")
+  
+if gui.actvt():
+  showGUI(option1("Report to AEGiS Corperation®"),nop(3))
+                  printScreen("Thank you for reporting an error.")
+                  print("Reported.")
+    option2("Cancel",nop(3))
+            printScreen("The action has been cancelled.")
+      print("The action has been cancelled.")
